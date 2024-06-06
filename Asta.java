a1bfdad7710153d1329a61a2df9386757804b674
Kilotas <Alendos87@gmail.com>
2024-06-06 18:45:44 +0500

    Добавил readme.md

--- Asta.java
+++ Asta.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Asta {
     public static void main(String[] args) {
-        System.out.println("Hello Asta");
+        System.out.println("Hello Yuno");
 
     }
 }
